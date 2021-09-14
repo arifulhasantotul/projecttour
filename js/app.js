6b4fed6d97d6f157aec961e22d63a38dc90acf02
arifulhasantotul <77568259+arifulhasantotul@users.noreply.github.com>
2021-09-14 12:50:54 +0600

    login form done

--- js/app.js
+++ js/app.js
@@ -1,12 +1,30 @@
 const searchBtn = document.getElementById("search_btn");
 const searchBar = document.querySelector(".search_bar_container");
 
+const formBtn = document.getElementById("user_btn");
+const loginForm = document.querySelector(".login_form_container");
+const formClose = document.getElementById("form_close");
+
 window.onscroll = () => {
     searchBtn.classList.remove("fa-times");
     searchBar.classList.remove("active");
 };
 
+const btnToggle = (btnName, containerName) => {
+    btnName.classList.toggle("fa-times");
+    containerName.classList.toggle("active");
+};
+
 searchBtn.addEventListener("click", () => {
-    searchBtn.classList.toggle("fa-times");
-    searchBar.classList.toggle("active");
+    // searchBtn.classList.toggle("fa-times");
+    // searchBar.classList.toggle("active");
+    btnToggle(searchBtn, searchBar);
+});
+
+formBtn.addEventListener("click", () => {
+    loginForm.classList.add("active");
+});
+
+formClose.addEventListener("click", () => {
+    loginForm.classList.remove("active");
 });
