cf6055331359068f533e04cba67c8c1acafcfa89
arifulhasantotul <77568259+arifulhasantotul@users.noreply.github.com>
2021-09-15 14:15:16 +0600

    brnad done

--- js/initSwiper.js
+++ js/initSwiper.js
@@ -1,3 +1,4 @@
+
 let swiperComment = new Swiper(".review_slider", {
     spaceBetween: 20,
     centeredSlides: true,
@@ -15,6 +16,31 @@ let swiperComment = new Swiper(".review_slider", {
         },
         1024: {
             slidesPerView: 3,
+        },
+    },
+});
+
+let swiperBrand = new Swiper(".brand_slider", {
+    spaceBetween: 20,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    breakpoints: {
+        450: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        991: {
+            slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
         },
     },
 });
